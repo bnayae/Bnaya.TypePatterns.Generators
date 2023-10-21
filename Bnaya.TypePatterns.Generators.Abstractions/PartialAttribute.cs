18bc195f787d32a3e26b9a05c71394e9946396ef
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2023-10-21 08:48:01 +0300

    feat: partial

--- Bnaya.TypePatterns.Generators.Abstractions/PartialAttribute.cs
+++ Bnaya.TypePatterns.Generators.Abstractions/PartialAttribute.cs
@@ -1,7 +1,7 @@
 ﻿namespace Bnaya.TypePatterns.Generators;
 
 /// <summary>
-/// Code generation Partial functionality, mimik the TypeScript feature: https://www.typescriptlang.org/docs/handbook/utility-types.html#omittype-keys
+/// Code generation Partial functionality, mimik the TypeScript feature: https://www.typescriptlang.org/docs/handbook/utility-types.html#Partialtype-keys
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
 public class PartialAttribute : Attribute
